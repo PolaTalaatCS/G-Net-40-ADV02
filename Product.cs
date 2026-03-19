e73c98224af77e75f8137ca7e67a73e9b8131f97
PolaTalaatCS <polatalaatcs7@gmail.com>
2026-03-19 17:20:01 +0200

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_Net_40_ADV02
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; } // "Electronics", "Clothing", "Food", "Books" 
+        public double Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
---
+++ Search.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_Net_40_ADV02
+{
+    public class Search
+    {
+        public static List<Product> SearchProduct(List<Product> products, Func<Product, bool> selector)
+        {
+            List<Product> result = new List<Product>();
+            foreach (Product product in products)
+            {
+                if (selector(product))
+                {
+                    result.Add(product);
+                }
+                
+            }
+            return result;
+        }
+    }
+}
